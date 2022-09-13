0f2af4903f34aa32bd5a29b28ca7cda51905208c
Caroline Bridge <cbridge@oicr.on.ca>
2022-09-13 13:47:54 -0400

    rm lazy loading

--- src/LinearGenomeMultilevelView/index.ts
+++ src/LinearGenomeMultilevelView/index.ts
@@ -1,15 +1,13 @@
-import { lazy } from 'react'
 import PluginManager from '@jbrowse/core/PluginManager'
 import modelFactory from './model'
+import ReactComponent from './components/LinearGenomeMultilevelView'
 
 export default ({ jbrequire }: PluginManager) => {
   const ViewType = jbrequire('@jbrowse/core/pluggableElementTypes/ViewType')
   return new ViewType({
     name: 'LinearGenomeMultilevelView',
     stateModel: jbrequire(modelFactory),
-    ReactComponent: lazy(
-      () => import('./components/LinearGenomeMultilevelView'),
-    ),
+    ReactComponent,
     extendedName: 'LinearGenomeView',
   })
 }
--- src/MultilevelLinearView/index.ts
+++ src/MultilevelLinearView/index.ts
@@ -1,12 +1,12 @@
-import { lazy } from 'react'
 import PluginManager from '@jbrowse/core/PluginManager'
 import modelFactory from './model'
+import ReactComponent from './components/MultilevelLinearView'
 
 export default ({ jbrequire }: PluginManager) => {
   const ViewType = jbrequire('@jbrowse/core/pluggableElementTypes/ViewType')
   return new ViewType({
     name: 'MultilevelLinearView',
     stateModel: jbrequire(modelFactory),
-    ReactComponent: lazy(() => import('./components/MultilevelLinearView')),
+    ReactComponent,
   })
 }
